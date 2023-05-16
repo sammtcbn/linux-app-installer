54d380bfd47efd52b3c1e63730d02ac95f781d14
sammtcbn <sammtcbn@gmail.com>
2023-05-16 14:50:12 +0800

    minior update

--- chrome-install.bash
+++ chrome-install.bash
@@ -6,7 +6,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb |
 
 echo
 echo dpkg -i google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb || exit 1
+dpkg -i google-chrome-stable_current_amd64.deb || exit 1
 
 echo
 echo rm -f google-chrome-stable_current_amd64.deb
